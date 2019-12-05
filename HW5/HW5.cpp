a57f074b7b315fc7141aada0449b9524ecac013f
Niraj Mali <nirajlmali@gmail.com>
2019-12-05 00:57:17 -0800

    Added parallelLCS method, need to add parallelism to code

--- HW5/HW5.cpp
+++ HW5/HW5.cpp
@@ -63,6 +63,63 @@ void serialLCS(string A, string B) {
 	}
 }
 
+//Parallelized
+void parallelLCS(string A, string B) {
+	//Size of each
+	int m = A.length();
+	int n = B.length();
+
+	//Length of each current subsequence
+	int len1 = 0;
+	int len2 = 0;
+
+	//L - Data Structure made up of subsequences
+	//Simple parallelization
+	string** L = new string * [m + 1];
+	for (int i = 0; i < m + 1; i++)
+	{
+		L[i] = new string[n + 1];
+	}
+
+	//Locks? L[0][0], L[1][0], L[0][1] are our initial cases
+
+	//Threadex.
+	//TODO launch 3 threads?
+	/*std::thread th1(insertintosl, &list, 5000);
+	std::thread th2(insertintosl, &list, 5000);
+	std::thread th3(searchskiplist, &list);*/
+
+	/* Careful of i-1, j-1 < 0! */
+	for (int i = 0; i < m + 1; i++) {
+		for (int j = 0; j < n + 1; j++) {
+			if (i <= 0 || j <= 0) {
+				L[i][j] = "";
+			}
+			else if (A[i - 1] == B[j - 1]) {
+				/* L[i,j] = L(A_i, B_j) */
+				L[i][j] = L[i - 1][j - 1] + "" + A[i - 1]; // Concatenate A[i] to L[i-1,j-1]
+			}
+			else {
+				len1 = L[i][j - 1].length();
+				len2 = L[i - 1][j].length();
+				if (len1 > len2)
+					L[i][j] = L[i][j - 1];
+				else
+					L[i][j] = L[i - 1][j];
+			}
+			cout << L[i][j] << "\n";
+		}
+	}
+	cout << "Final Table";
+	for (int i = 0; i < m + 1; i++) {
+		for (int j = 0; j < n + 1; j++) {
+			cout << L[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+
 int main()
 {
 	char A[] = "ABXTC";
